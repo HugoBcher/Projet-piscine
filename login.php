6047b26ef7199ec2c3ee1bd37c4abf0986d6e6d6
Hugocentrique8376 <hugo.boucher@edu.ece.fr>
2019-03-19 14:35:16 +0100

    last

--- login.php
+++ login.php
@@ -22,8 +22,14 @@ session_start();
                              if($_POST['email']== $db_field['email'] && $_POST['password']==$db_field['pseudo']){
                                     
                                   $_SESSION['pseudo'] = $_POST['password'];
-                                    echo '<meta http-equiv="refresh" content="0;URL=index.php">';
+                                 if($db_field['type']==1){
+                                    echo '<meta http-equiv="refresh" content="0;URL=indexL.php">';
                                  $co=1;
+                                 }
+                                 if($db_field['type']==0){
+                                    echo '<meta http-equiv="refresh" content="0;URL=indexT.php">';
+                                 $co=1;
+                                 }
                              }
                              else if($_POST['email']!= $db_field['email'] || $_POST['password']!=$db_field['pseudo'])
                             {
